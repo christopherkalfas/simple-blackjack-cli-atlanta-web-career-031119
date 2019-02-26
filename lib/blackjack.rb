f1fbd26ee33e7fcc7f3f43aaa83c6ffe637f9dd3
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-26 22:02:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,9 +55,9 @@ def runner
   hit?(card_total)
   display_card_total(card_total)
    
-  while card_total <= 21
-    hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game
+  # while card_total <= 21
+  #   hit?(card_total)
+  #   display_card_total(card_total)
+  # end
+  # end_game
 end 
